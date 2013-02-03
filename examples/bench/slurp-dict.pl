5b7147aa1e11e766dd2d94ab29d6bee8fc94d478
David Golden <dagolden@cpan.org>
2013-02-03 04:12:32 -0500

    add large slurp benchmark

---
+++ examples/bench/slurp-dict.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Benchmark qw( cmpthese :hireswallclock );
+use Path::Tiny;
+use Path::Class;
+use File::Fu;
+use IO::All;
+
+print "$0\n";
+
+my $file = "/usr/share/dict/words";
+
+my $count = -1;
+cmpthese(
+    $count,
+    {
+        'Path::Tiny'  => sub { my $s = path($file)->slurp },
+        'Path::Class' => sub { my $s = file($file)->slurp },
+        'IO::All'     => sub { my $s = io($file)->slurp },
+        'File::Fu'    => sub { my $s = File::Fu->file($file)->read },
+    }
+);
+
