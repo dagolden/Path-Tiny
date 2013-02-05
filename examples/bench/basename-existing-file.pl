050b63865f68b8f8c2b4a11b93f9b1972ff793fa
David Golden <dagolden@cpan.org>
2013-02-05 11:51:05 -0500

    change benchmark scripts to Benchmark::Forking

--- examples/bench/basename-existing-file.pl
+++ examples/bench/basename-existing-file.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use Benchmark qw( cmpthese :hireswallclock );
+use Benchmark::Forking qw/cmpthese/;
 use Path::Tiny;
 use Path::Class;
 use File::Fu;
