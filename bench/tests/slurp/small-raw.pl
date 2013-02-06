fdd241830660a7a0a66d9a5180163b4eb7a93bb6
David Golden <dagolden@cpan.org>
2013-02-06 04:20:17 -0500

    fix slurp-raw benchmark test

--- bench/tests/slurp/small-raw.pl
+++ bench/tests/slurp/small-raw.pl
@@ -4,7 +4,7 @@ my $result = timethese (
     $count,
     {
         'Path::Tiny'  => sub { my $s = path($file)->slurp_raw },
-        'Path::Class' => sub { my $s = file($file)->slurp( iomode => ":raw" ) },
+        'Path::Class' => sub { my $s = file($file)->slurp( iomode => "<:raw" ) },
         'IO::All'     => sub { my $s = io($file)->binary->slurp; },
         'File::Fu'    => sub { my $s = File::Fu->file($file)->read( {binmode => ":raw"}) },
     },
