ddeabddbc25749033dee1a3dd82972e940ec096e
David Golden <dagolden@cpan.org>
2013-02-07 15:41:29 -0500

    shut up Perl::Critic

--- lib/Path/Tiny.pm
+++ lib/Path/Tiny.pm
@@ -472,7 +472,7 @@ sub lines_utf8 {
     $_[1] = {} unless ref $_[1] eq 'HASH';
     if ( $HAS_UU //= eval { require Unicode::UTF8; 1 } && !$_[1]->{count} ) {
         # when we split, we lose the \n, so put *back* the \n if not chomping
-        return map { $_[1]->{chomp} ? $_ : ($_ .= "\n") } split /\n/, slurp_utf8( $_[0] );
+        return map { $_[1]->{chomp} ? $_ : ($_ .= "\n") } split /\n/, slurp_utf8( $_[0] ); ## no critic
     }
     else {
         $_[1]->{binmode} = ":raw:encoding(UTF-8)";
