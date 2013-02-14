f37c44d405adcd928e0824fac40e022f8d82e0b1
David Golden <dagolden@cpan.org>
2013-02-13 23:27:51 -0500

    more attempts at efficient lines_utf8

--- lib/Path/Tiny.pm
+++ lib/Path/Tiny.pm
@@ -474,10 +474,8 @@ and iterating directly on the handle.
 
 sub lines_utf8 {
     $_[1] = {} unless ref $_[1] eq 'HASH';
-    if ( ( $HAS_UU //= _check_UU() ) && !$_[1]->{count} ) {
-        # when we split, we lose the \n, so put *back* the \n if not chomping
-        return map { $_[1]->{chomp} ? $_ : ( $_ .= "\n" ) } split /\n/,
-          slurp_utf8( $_[0] ); ## no critic
+    if ( ( $HAS_UU //= _check_UU() ) && $_[1]->{chomp} && !$_[1]->{count} ) {
+        return split /\n/, slurp_utf8( $_[0] ); ## no critic
     }
     else {
         $_[1]->{binmode} = ":raw:encoding(UTF-8)";
