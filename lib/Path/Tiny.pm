11349d40d5248b29fdf1fb7b21cf941deb5b647f
Shlomi Fish <shlomif@shlomifish.org>
2015-12-27 19:47:25 +0200

    Correct a double "equivalent" word.
    This was a typo. I hereby disclaim all copyrights ownership on the code
    - as always.

--- lib/Path/Tiny.pm
+++ lib/Path/Tiny.pm
@@ -1749,7 +1749,7 @@ sub visit {
     $vol = path("C:/tmp/foo.txt")->volume; # "C:"
 
 Returns the volume portion of the path.  This is equivalent
-equivalent to what L<File::Spec> would give from C<splitpath> and thus
+to what L<File::Spec> would give from C<splitpath> and thus
 usually is the empty string on Unix-like operating systems or the
 drive letter for an absolute path on C<MSWin32>.
 
